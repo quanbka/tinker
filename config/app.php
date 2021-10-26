c8da42903fc1ad1960050d76bbdaec994831f96d
Le Van Quan <quanbka.cntt@gmail.com>
2021-10-26 15:00:53 +0700

    init: theme selector

--- config/app.php
+++ config/app.php
@@ -176,6 +176,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Igaster\LaravelTheme\themeServiceProvider::class,
+
 
     ],
 
@@ -225,6 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Theme' => Igaster\LaravelTheme\Facades\Theme::class,
+
 
     ],
 
